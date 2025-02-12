e75935ab2d777baec72ddcc771d52b15f84deb23
Paulo <paul.brto@gmail.com>
2025-02-11 21:08:53 -0300

    feat: Pong

---
+++ Pong/Assets/BallControl.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class BallControl : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private Rigidbody2D rb2d;         
+    
+    void GoBall(){                      
+        float rand = Random.Range(0, 2);
+        if(rand < 1){
+            rb2d.AddForce(new Vector2(20, -15));
+        } else {
+            rb2d.AddForce(new Vector2(-20, -15));
+        }
+    }
+
+    void OnCollisionEnter2D (Collision2D coll) {
+        if(coll.collider.CompareTag("Player")){
+            Vector2 vel;
+            vel.x = rb2d.linearVelocity.x;
+            vel.y = (rb2d.linearVelocity.y / 2) + (coll.collider.attachedRigidbody.linearVelocity.y / 3);
+            rb2d.linearVelocity = vel;
+        }
+    }
+
+
+    void ResetBall(){
+        rb2d.linearVelocity = Vector2.zero;
+        transform.position = Vector2.zero;
+    }
+
+    // Reinicializa o jogo
+    void RestartGame(){
+        ResetBall();
+        Invoke("GoBall", 1);
+    }
+
+      // Define o corpo rigido 2D que representa a bola
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>(); // Inicializa o objeto bola
+        Invoke("GoBall", 2);    // Chama a função GoBall após 2 segundos
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
